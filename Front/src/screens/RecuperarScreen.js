93020c9cd9118231b4de40d589cbaa9152770f58
RubenNH <104938082+RubenNH@users.noreply.github.com>
2023-04-06 15:27:38 -0600

    rec datos spring

--- Front/src/screens/RecuperarScreen.js
+++ Front/src/screens/RecuperarScreen.js
@@ -1,24 +1,60 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
 import axios from 'axios';
 
 export default function RecuperarScreen() {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    axios.get('http://localhost:8080/api-seda/piso/1')
-      .then(response => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://192.168.100.5:8080/api-seda/sensor/');
         setData(response.data);
-      })
-      .catch(error => {
+        console.log(data);
+      } catch (error) {
         console.error(error);
-      });
+      }
+    };
+
+    fetchData();
   }, []);
 
-  if (!data) {
-    return <></>;
-  }
+  const renderItem = ({ item }) => (
+    <View style={styles.item}>
+      <Text style={styles.title}>{item.title}</Text>
+      <Text>{item.description}</Text>
+    </View>
+  );
 
   return (
-    <div></div>
+    <View style={styles.container}>
+      {data ? (
+        <FlatList
+          data={data}
+          renderItem={renderItem}
+          keyExtractor={item => item.id.toString()}
+        />
+      ) : (
+        <Text>Loading data...</Text>
+      )}
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 24,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+});
